2a6864f2fbb89185f25429ef98edb5911e6ecd10
CHARAFI Saad <mrsaad.charafi@gmail.com>
2018-06-23 11:48:43 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServeursGestionStock/ServeursGestionStockApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.ServeursGestionStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeursGestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeursGestionStockApplication.class, args);
+	}
+}
